658092313cff5b5648ff991320cc5e64f3532661
Greg Allen <me@jga.me>
2011-09-08 07:57:43 -0700

    updated unit tests

--- tests/ga.test.js
+++ tests/ga.test.js
@@ -5,35 +5,36 @@ var testHost = 'nodega.jga.me';
 
 module = QUnit.module;
 module('ga', {
-	setup: function() {
-		this.ga = new GoogleAnalytics(testUA, testHost);
-		this.ga.debug = true;
-	},
-	teardown: function() {
-	}
+  setup: function() {
+    this.ga = new GoogleAnalytics(testUA, testHost);
+  },
+  teardown: function() {
+  }
 });
 
 test('test', function() {
-	ok(true);
-});
-
-asyncTest('pageview', function() {
-	expect(1);
-	this.ga.trackPage('testing/1', function() {
-		ok(true);
-	});
+  ok(true);
 });
 
 test('userId', function() {
-	var userId = this.ga.trackPage('user/1');
-	ok(userId);
+  var userId = this.ga.trackPage('user/1');
+  ok(userId);
+});
+
+asyncTest('pageview', function() {
+  expect(1);
+  this.ga.trackPage('testing/1', function() {
+    ok(true);
+    start();
+  });
 });
 
 asyncTest('test user', function() {
-	expect(1);
-	var userId = this.ga.trackPage('user/1');
-	this.ga.trackPage('user/2', userId, function() {
-		ok(true);
-	});
+  expect(1);
+  var userId = this.ga.trackPage('user/1');
+  this.ga.trackPage('user/2', userId, function() {
+    ok(true);
+    start();
+  });
 });
 
